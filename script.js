2391e3242c4555a43aefe1c1a69599417b637da0
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-02-11 22:36:40 +0600

    Class-35

---
+++ script.js
@@ -0,0 +1,33 @@
+ marks = 66
+
+      
+
+        if(marks>100){
+            console.log("Invalid Marks");
+        }
+        else if(marks<0){
+            console.log("Invalid Marks");
+        }
+        else if(marks>=80){
+            console.log("The grade is A+");
+        }
+        else if(marks>=70){
+            console.log("The grade ia A");
+        }
+        else if(marks>=60){
+            console.log("The grade is A-");
+        }
+        else if(marks>=50){
+            console.log("The grade is B");
+        }
+        else if(marks>=40){
+            console.log("The grade is C");
+        }
+        else if(marks>=33){
+            console.log("The grade is D");
+        }
+        else{
+            console.log("The grade is F");
+        }
+
+
